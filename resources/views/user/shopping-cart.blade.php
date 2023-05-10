1787624f40fb8fc34283afa99bb43f3bf74ade2b
praabima <100179685+praabima@users.noreply.github.com>
2023-05-10 17:11:49 +0700

    terbaru

--- resources/views/user/shopping-cart.blade.php
+++ resources/views/user/shopping-cart.blade.php
@@ -1,131 +1,4 @@
-
-<<<<<<< HEAD
 @extends('user.layout.main')
-=======
-<!DOCTYPE html>
-<html lang="zxx">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Male_Fashion Template">
-    <meta name="keywords" content="Male_Fashion, unica, creative, html">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Owari Store | Official</title>
-
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
-        rel="stylesheet">
-
-    <!-- Css Styles -->
-    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('frontend/css/font-awesome.min.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('frontend/css/elegant-icons.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('frontend/css/magnific-popup.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('frontend/css/nice-select.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('frontend/css/slicknav.min.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}" type="text/css">
-</head>
-
-<body>
-    <!-- Page Preloder -->
-    <div id="preloder">
-        <div class="loader"></div>
-    </div>
-
-    <!-- Offcanvas Menu Begin -->
-    <div class="offcanvas-menu-overlay"></div>
-    <div class="offcanvas-menu-wrapper">
-        <div class="offcanvas__option">
-            <div class="offcanvas__links">
-                <a href="login/login.html">Log in</a>
-
-            </div>
-            <div class="offcanvas__top__hover">
-
-            </div>
-        </div>
-        <div class="offcanvas__nav__option">
-            <a href="#" class="search-switch"><img src="{{ asset('frontend/img/icon/search.png') }}" alt=""></a>
-            <a href="#"><img src="{{ asset('frontend/img/icon/heart.png') }}" alt=""></a>
-            <a href="shopping-cart.html"><img src="{{ asset('frontend/img/icon/cart.png') }}" alt="">
-                <span>0</span></a>
-            <div class="price">Rp0.00</div>
-        </div>
-        <div id="mobile-menu-wrap"></div>
-        <div class="offcanvas__text">
-
-        </div>
-    </div>
-    <!-- Offcanvas Menu End -->
-
-    <!-- Header Section Begin -->
-    <header class="header">
-        <div class="header__top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 col-md-7">
-                        <div class="header__top__left">
-
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-5">
-                        <div class="header__top__right">
-                            <div class="header__top__links">
-                                <a href="login/login.html">Log in</a>
-
-                            </div>
-                            <div class="header__top__hover">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-3">
-                    <div class="header__logo">
-                        <a href="./index.html"><img src="{{ asset('frontend/img/logo.png') }}" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6">
-                    <nav class="header__menu mobile-menu">
-                        <ul>
-                            <li><a href="./index.html">Home</a></li>
-                            <li><a href="./shop.html">Shop</a></li>
-                            <li><a href="#">Pages</a>
-                                <ul class="dropdown">
-                                    <li><a href="./about.html">About Us</a></li>
-                                    <li><a href="./shop-details.html">Shop Details</a></li>
-                                    <li><a href="./shopping-cart.html">Shopping Cart</a></li>
-                                    <li><a href="./checkout.html">Check Out</a></li>
-                                    <li><a href="./blog-details.html">Blog Details</a></li>
-                                </ul>
-                            </li>
-                            <li class="active"><a href="./blog.html">Blog</a></li>
-                            <li><a href="./contact.html">Contacts</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div class="col-lg-3 col-md-3">
-                    <div class="header__nav__option">
-                        <a href="#" class="search-switch"><img src="{{ asset('frontend/img/icon/search.png') }}"
-                                alt=""></a>
-                        <a href="#"><img src="{{ asset('frontend/img/icon/heart.pn') }}g" alt=""></a>
-                        <a href="shopping-cart.html"><img src="{{ asset('frontend/img/icon/cart.png') }}" alt="">
-                            <span>0</span></a>
-                        <div class="price">Rp0.00</div>
-                    </div>
-                </div>
-            </div>
-            <div class="canvas__open"><i class="fa fa-bars"></i></div>
-        </div>
-    </header>
-    <!-- Header Section End -->
->>>>>>> a09911e24cee037014aacbd41aa90cd46f3f5ce5
 
 @section('content')
     <!-- Breadcrumb Section Begin -->
@@ -337,13 +210,8 @@
                             <script>
                             document.write(new Date().getFullYear());
                             </script>2020
-<<<<<<< HEAD
-                            All rights reserved | This template is made with <i class="fa fa-heart-o"
-                                aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-=======
                             Owari Store | Official <i class="fa fa-heart-o" aria-hidden="true"></i> by <a
                                 href="https://colorlib.com" target="_blank">Colorlib</a>
->>>>>>> a09911e24cee037014aacbd41aa90cd46f3f5ce5
                         </p>
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </div>
@@ -363,22 +231,6 @@
         </div>
     </div>
     <!-- Search End -->
-<<<<<<< HEAD
+
 @endsection
-=======
 
-    <!-- Js Plugins -->
-    <script src="{{ asset('frontend/js/jquery-3.3.1.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/jquery.nice-select.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/jquery.nicescroll.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/jquery.magnific-popup.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/jquery.countdown.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/jquery.slicknav.js') }}"></script>
-    <script src="{{ asset('frontend/js/mixitup.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/main.j') }}s"></script>
-</body>
-
-</html>
->>>>>>> a09911e24cee037014aacbd41aa90cd46f3f5ce5
